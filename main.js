5fc238dd18115f49689aa148629b9c03b502eae9
luiscaputo0 <andersonfranciscoig@gmail.com>
2024-05-17 13:31:54 +0100

    feat: teste

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
 const fs = require('fs');
-const lexer = require('./lexer');
+const lexer = require('./Analisadores/lexer');
 
-// Ler o arquivo de código-fonte
 const codigoFonte = fs.readFileSync('codigo.c', 'utf-8');
 
-// Executar o analisador léxico
 const tokens = lexer(codigoFonte);
+
+console.log(tokens)
 
 console.log("\n");
 console.log("LEXEMA".padEnd(80), "TOKEN".padEnd(40), "Linha");
-console.log("\n");
 
 tokens.forEach(token => {
     console.log(token.value.padEnd(80), token.type.padEnd(40), token.line);
-    console.log("\n");
 });
