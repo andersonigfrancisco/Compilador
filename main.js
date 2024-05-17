63238f4d54d71146745f9eb6893f15ffd8737073
luiscaputo0 <andersonfranciscoig@gmail.com>
2024-05-17 13:53:42 +0100

    feat: update main

--- main.js
+++ main.js
@@ -2,14 +2,11 @@ const fs = require('fs');
 const lexer = require('./Analisadores/lexico');
 
 const codigoFonte = fs.readFileSync('codigo.c', 'utf-8');
-
 const tokens = lexer(codigoFonte);
 
-console.log(tokens)
-
 console.log("\n");
-console.log("LEXEMA".padEnd(80), "TOKEN".padEnd(40), "Linha");
+console.log("LEXEMA".padEnd(80), "TOKEN".padEnd(40), "LINHA");
 
 tokens.forEach(token => {
-    console.log(token.value.padEnd(80), token.type.padEnd(40), token.line);
+    console.log(token.value.padEnd(80), token.type.padEnd(42), token.line);
 });
